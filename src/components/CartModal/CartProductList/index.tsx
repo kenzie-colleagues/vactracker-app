5193857e205ae9370067a19ca112691c77a8e90c
marigv95 <mama_guima04@hotmail.com>
2023-03-09 14:07:41 -0300

    Criar carrinho de compras

---
+++ src/components/CartModal/CartProductList/index.tsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import CartProductCard from "./CartProductCard";
+
+import { IVaccines } from "../../../providers/@types";
+import { CartContext } from "../../../providers/CartContext/CartContext";
+
+const CartProductList = () => {
+  const { shoppingCartList, valueCart, removeAllItens } =
+    useContext(CartContext);
+
+  return (
+    <div>
+      <ul>
+        {shoppingCartList.map(
+          (product: IVaccines) =>
+            product.name && (
+              <CartProductCard key={product.id} product={product} />
+            )
+        )}
+      </ul>
+
+      <div>
+        <p>
+          <strong>Total</strong>
+        </p>
+        <p>R$ {valueCart.toFixed(2)}</p>
+      </div>
+      <button onClick={() => removeAllItens()}>Remover todos</button>
+    </div>
+  );
+};
+
+export default CartProductList;
---
+++ src/providers/CartContext/@typesCart.ts
@@ -0,0 +1,24 @@
+import { IVaccines } from "../@types";
+
+export interface IDefaultProviderProps {
+  children: React.ReactNode;
+}
+
+export interface IProductsProps {
+  product: IVaccines;
+}
+
+export interface ICartContext {
+  cart: IVaccines[];
+  setCart: React.Dispatch<React.SetStateAction<IVaccines[]>>;
+  // modalCartShoppingList: boolean;
+  // setModalCartShoppingList: React.Dispatch<React.SetStateAction<boolean>>;
+  addCartShopping: (product: IVaccines) => void;
+  searchMenuList: IVaccines[];
+  setShoppingCartList: React.Dispatch<any>;
+  shoppingCartList: any;
+  valueCart: any;
+  removeAllItens: () => void;
+  removeCartShopping: (productId: any) => void;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
