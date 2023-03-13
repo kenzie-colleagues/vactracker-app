11910928ec42e2aa2392420a7a618d1b7f409e69
NetoBasso <baxnetodev@gmail.com>
2023-03-13 14:23:44 -0300

    Fix Footer stylization

--- src/components/FooterHome/index.tsx
+++ src/components/FooterHome/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
-/*import ClientMail from '../../components/FooterDash/Mensage'*/
+import FaleConoscoImg from '../../assets/FaleConosco.png';
+
 const FooterHomePage = () => {
   return (
-    <footer className=" flex flex-row bg-blue-100 sm:h-87 text-sm   py-4 px-6">
-      <div className=' flex flex-col'>
-      <span className='text-emerald-800'>Central de Atendimento: 0800-1234-5678</span>
+    <footer className="flex flex-row items-center justify-center bg-blue-100 sm:h-87 text-sm py-4 px-6">
+      <div className='flex flex-col'>
+        <span className='text-emerald-800'>Central de Atendimento: 0800-1234-5678</span>
         <span className='text-emerald-800'>Atendimento 24 horas</span>
       </div>
       <div>
-      <Link className='  ml-64 '   to={"/mail"}>Ajuda</Link>
+        <Link className='ml-64' to={"/mail"}>
+          <img src={FaleConoscoImg} alt="Fale Conosco" className="inline-block h-12 w-12" />
+        </Link>
       </div>
     </footer>
-  )
-}
+  );
+};
+
 export default FooterHomePage;
+
+
